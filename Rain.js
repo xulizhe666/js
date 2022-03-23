5c6317633198995481e1b5630e4c5d39dca99694
xulizhe666 <68450475+xulizhe666@users.noreply.github.com>
2022-03-23 20:10:53 +0800

    Update Rain.js

--- Rain.js
+++ Rain.js
@@ -1,4 +1,13 @@
+/*
 
+[rewrite_local]
+
+url script-response-body
+
+[mitm]
+
+
+*/
 let obj = JSON.parse($response.body);
 obj.data.premium_promo["settings"] = true;
 $done({body: JSON.stringify(obj)});
