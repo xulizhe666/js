1616ef421e51556b8521256f4e4bc7c360c8819d
xulizhe666 <68450475+xulizhe666@users.noreply.github.com>
2023-09-12 12:20:12 +0800

    Update exue.js

--- exue.js
+++ exue.js
@@ -1,6 +1,17 @@
+/***
+[rewrite_local]
+
+https?:\/\/ele\.chng\.com\.cn\/api\/ams\/lms\/learnlesson\/userlearnlesson\/uploadLearnData url script-request-body https://github.com/xulizhe666/js/raw/main/exue.js
+
+[mitm] 
+
+hostname = ele.chng.com.cn
+***/
+
 let body = $request.body;
 
 body = body.replace("\"finished\": 0", "\"finished\": 1");
 console.log(body)
 
+$notify('成功修改', '已完成', `1`)
 $done({body});
