86c8f7388cd0535f176e9abc44f953e57c6556c1
xulizhe666 <68450475+xulizhe666@users.noreply.github.com>
2023-09-12 12:27:59 +0800

    Update exue.js

--- exue.js
+++ exue.js
@@ -10,7 +10,7 @@ hostname = ele.chng.com.cn
 
 let body = $request.body;
 
-body = body.replace("\"finished\": 0", "\"finished\": 1");
+body = body.replace("\"finished\":0", "\"finished\":1");
 console.log(body)
 
 $notify('成功修改', '已完成', `1`)
